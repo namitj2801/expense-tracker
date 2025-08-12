348883d0a70de6f1db1ffe5d36aecbe28c7b256a
namitj2801 <namitj2801@gmail.com>
2025-08-13 03:41:40 +0530

    Refactor data.js for improved code readability: standardize formatting and indentation in the SIDE_MENU_DATA export.

--- frontend/expense-tracker/src/Utils/data.js
+++ frontend/expense-tracker/src/Utils/data.js
@@ -1,35 +1,35 @@
-    import {
-    LuLayoutDashboard,
-    LuHandCoins,
-    //   LuWallMinimal,
-    LuLogOut,
-    LuWalletMinimal,
-    } from "react-icons/lu";
+import {
+  LuLayoutDashboard,
+  LuHandCoins,
+  //   LuWallMinimal,
+  LuLogOut,
+  LuWalletMinimal,
+} from "react-icons/lu";
 
-    export const SIDE_MENU_DATA = [
-    {
-        id: "01",
-        label: "Dashboard",
-        icon: LuLayoutDashboard,
-        path: "/dashboard",
-    },
-    {
-        id: "02",
-        label: "Income",
-        icon: LuWalletMinimal,
-        path: "/income",
-    },
-    {
-        id: "03",
-        label: "Expense",
-        icon: LuHandCoins,
-        path: "/expense",
-    },
+export const SIDE_MENU_DATA = [
+  {
+    id: "01",
+    label: "Dashboard",
+    icon: LuLayoutDashboard,
+    path: "/dashboard",
+  },
+  {
+    id: "02",
+    label: "Income",
+    icon: LuWalletMinimal,
+    path: "/income",
+  },
+  {
+    id: "03",
+    label: "Expense",
+    icon: LuHandCoins,
+    path: "/expense",
+  },
 
-    {
-        id: "06",
-        label: "Logout",
-        icon: LuLogOut,
-        path: "logout",
-    },
-    ];
+  {
+    id: "06",
+    label: "Logout",
+    icon: LuLogOut,
+    path: "logout",
+  },
+];
