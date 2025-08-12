469b998da83eff3d6192f1fd1fa26ce067cb5b7a
namitj2801 <namitj2801@gmail.com>
2025-08-13 03:39:21 +0530

    Enhance Navbar and SideMenu components: implement responsive side menu toggle, integrate user context for profile display, and add navigation functionality with logout handling.

--- frontend/expense-tracker/src/components/layouts/SideMenu.jsx
+++ frontend/expense-tracker/src/components/layouts/SideMenu.jsx
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { SIDE_MENU_DATA } from "../../Utils/data";
+import { UserContext } from "../../context/UserContext";
 
-const SideMenu = () => {
+const SideMenu = ({ activeMenu }) => {
+  const { user, clearUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  console.log("activeMenu prop:", activeMenu);
+  console.log("location.pathname:", location.pathname);
+  // console.log("item.path:", item.path);
+  // console.log("Is active?", location.pathname === item.path);
+
+  const handleClick = (route) => {
+    if (route === "logout") {
+      handleLogout();
+      return;
+    }
+
+    navigate(route);
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    clearUser();
+    navigate("/login");
+  };
+
   return (
-    <div>
-      <p>SideMenu</p>
+    <div className="w-64 h-[calc(100vh-61px)] bg-white border-r border-gray-200/50 p-5 sticky top-[61px] z-20">
+      <div className="flex  flex-col items-center justify-center gap-3 mt-3 mb-7">
+        {user?.profileImageUrl ? (
+          <img
+            src={user?.profileImageUrl || ""}
+            alt="Profile Image"
+            className="w-20 h-20 bg-slate-400 rounded-full"
+          />
+        ) : (
+          <></>
+        )}
+        <h5 className="text-gray-950 font-medium leading-6">
+          {user?.fullName || ""}
+        </h5>
+      </div>
+      {SIDE_MENU_DATA.map((items, index) => {
+        return (
+          <button
+            key={`menu_${index}`}
+            className={`w-full flex items-center gap-4 text-[15px] ${
+              activeMenu == items.label ? "text-white bg-primary" : ""
+            } py-3 px-6 rounded-lg mb-3`}
+            onClick={() => handleClick(items.path)}
+          >
+            <items.icon className="text-xl" />
+            {items.label}
+          </button>
+        );
+      })}
     </div>
   );
 };
